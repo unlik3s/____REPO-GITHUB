f5845eab12e5e3764a63ffb45cf2675f674dd229
unlik3s <47140498+unlik3s@users.noreply.github.com>
2019-12-03 14:49:54 +0100

    Update Analyse_crypto_v1.0_GH.ps1

--- IIS_Crypt-usage/Analyse_crypto_v1.0_GH.ps1
+++ IIS_Crypt-usage/Analyse_crypto_v1.0_GH.ps1
@@ -1,10 +1,10 @@
 ################################################################################################################################################
 ## Author : Jeremy GARCIA
 ## Version : 1.0
-## Description: Analyse crypto used by clients parsing IIS log and/or SMTP LOG
+## Description: Analyse crypto used by clients parsing IIS log
 ## Prerequisites: Extended log must be enabled in IIS for detection of HTTPS cipher and log parser installed 
 ## https://www.microsoft.com/security/blog/2017/09/07/new-iis-functionality-to-help-identify-weak-tls-usage/
-## Usage : Example 1 : .\Analyse_crypto_v1.0.ps1 -HTTPCipher $true -SMTPCipher $false -AgeLog "-24" -jobsNumber 5 -Output "c:\Scripts\Results"
+## Usage : Example 1 : .\Analyse_crypto_v1.0.ps1 -HTTPCipher $true -AgeLog "-24" -jobsNumber 5 -Output "c:\Scripts\Results"
 ################################################################################################################################################
 
 param (
@@ -54,7 +54,6 @@ $srvprint = $servers -join ","
 Write-Host "#Option Servers : " -NoNewline;Write-Host "$srvprint" -ForegroundColor Yellow
 Write-Host "#Option Output : " -NoNewline;Write-Host "$Output" -ForegroundColor Yellow
 if ($HTTPCipher){Write-Host "#Option HTTPCipher : "-NoNewLine;Write-host "ON" -ForegroundColor Green}else{Write-Host "#Option HTTPCipher : " -NoNewLine;Write-Host "OFF" -ForegroundColor Red}
-if ($SMTPcipher){Write-Host "#Option SMTPCipher : " -NoNewLine;Write-Host "ON" -ForegroundColor Green}else{Write-Host "#Option SMTPCipher : " -NoNewLine;Write-Host "OFF" -ForegroundColor Red}
 Write-Host "**********************************************************************************************************"
 
 #HTTPCipher Code
